489a1c25bf665e34fa5a787accb2a7a5144fa371
Skro11 <bunetskydn@gmail.com>
2024-04-30 15:54:16 +0700

    feat(master): add pages and components

--- src/components/widgets/nav/links.ts
+++ src/components/widgets/nav/links.ts
@@ -1,18 +1,18 @@
-export const links = [
-	{
+export const links = {
+	home: {
 		title: 'Home',
 		link: '/',
 	},
-	{
+	gallery: {
 		title: 'Gallery',
 		link: '/gallery',
 	},
-	{
+	contact: {
 		title: 'Contact',
 		link: '/contact',
 	},
-	{
+	blog: {
 		title: 'Blog',
 		link: '/blog',
 	},
-];
+};
