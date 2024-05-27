2c8962df1ff95abc9e67ec7ff61120585bb89ace
Skro11 <bunetskydn@gmail.com>
2024-05-27 23:54:03 +0700

    feat(master): improve gallery, add normalize.css, change tariffs

--- config/links/social.ts
+++ config/links/social.ts
@@ -1,15 +1,34 @@
 type TSocialLink = {
 	icon: string;
-	title: string;
+	label: string;
 	link: string;
 	customClasses: string[];
 };
 
 export const socialLink: TSocialLink[] = [
 	{
-		icon: 'mdi:instagram',
-		title: 'Instagram Page',
+		icon: 'cib:instagram',
+		label: 'instagram',
 		link: 'https://www.instagram.com/nastifox13/',
+		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+	},
+	{
+		icon: 'cib:telegram',
+		label: 'telegram',
+		link: 'https://t.me/nasti_f13/',
+		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+	},
+
+	{
+		icon: 'cib:whatsapp',
+		label: 'whatsapp',
+		link: 'https://wa.me/79138202385/',
+		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+	},
+	{
+		icon: 'cib:vk',
+		label: 'vk',
+		link: 'https://vk.com/a.sudarchikova/',
 		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
 	},
 ];
