39623376ea6cd7e2ffb81eb6cb5b96fd0b3d8af6
Skro11 <bunetskydn@gmail.com>
2024-06-06 00:50:42 +0700

    feat(master): improve layout, add qr codes for contacts

--- config/links/social.ts
+++ config/links/social.ts
@@ -1,22 +1,90 @@
+import { getImage } from 'astro:assets';
+import wh from '../../public/photos/qr/wh.svg';
+import vk from '../../public/photos/qr/vk.svg';
+import inst from '../../public/photos/qr/inst.svg';
+import tg from '../../public/photos/qr/tg.svg';
+import ph from '../../public/photos/qr/ph.svg';
+import ml from '../../public/photos/qr/ml.svg';
+import type { GetImageResult } from 'astro';
+
+const vkQR = await getImage({
+	src: vk,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
+const whQR = await getImage({
+	src: wh,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
+const instQR = await getImage({
+	src: inst,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
+const tgQR = await getImage({
+	src: tg,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
+const phQR = await getImage({
+	src: ph,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
+const mlQR = await getImage({
+	src: ml,
+	format: 'webp',
+	inferSize: true,
+	quality: 100,
+});
+
 type TSocialLink = {
 	icon: string;
 	label: string;
 	link: string;
+	qr: GetImageResult;
 	customClasses: string[];
 };
 
 export const socialLink: TSocialLink[] = [
 	{
+		icon: 'mdi:phone',
+		label: 'phone',
+		link: 'tel:+79138202385',
+		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: phQR,
+	},
+	{
+		icon: 'mdi:email-outline',
+		label: 'mail',
+		link: 'mailto:ooo.basic@mail.ru',
+		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: mlQR,
+	},
+	{
 		icon: 'cib:instagram',
 		label: 'instagram',
 		link: 'https://www.instagram.com/nastifox13/',
 		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: instQR,
 	},
 	{
 		icon: 'cib:telegram',
 		label: 'telegram',
 		link: 'https://t.me/nasti_f13/',
 		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: tgQR,
 	},
 
 	{
@@ -24,11 +92,13 @@ export const socialLink: TSocialLink[] = [
 		label: 'whatsapp',
 		link: 'https://wa.me/79138202385/',
 		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: whQR,
 	},
 	{
 		icon: 'cib:vk',
 		label: 'vk',
 		link: 'https://vk.com/a.sudarchikova/',
 		customClasses: ['text-muted', 'hover:text-fuchsia-600'],
+		qr: vkQR,
 	},
 ];
