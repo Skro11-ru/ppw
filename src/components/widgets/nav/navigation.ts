a8c9a69d6fae1603b0cf7999692abcbaf5caab56
Skro11 <bunetskydn@gmail.com>
2024-10-28 12:05:48 +0700

    feat(master): change main and portfolio pages

--- src/components/widgets/nav/navigation.ts
+++ src/components/widgets/nav/navigation.ts
@@ -1,3 +1,11 @@
+import { getAllImages } from '@utils/getAllImages.ts';
+
+const getPoster = async (
+	category: 'man' | 'woman' | 'business' | 'other' | 'moto',
+) => {
+	return await getAllImages(category);
+};
+
 export const navigation = [
 	{
 		id: 'main',
@@ -12,22 +20,27 @@ export const navigation = [
 			{
 				title: 'Мужской портрет',
 				link: '/gallery/man/',
+				poster: await getPoster('man'),
 			},
 			{
 				title: 'Бизнес-портрет',
 				link: '/gallery/business/',
+				poster: await getPoster('business'),
 			},
 			{
 				title: 'Женский портрет',
 				link: '/gallery/woman/',
+				poster: await getPoster('woman'),
 			},
 			{
 				title: 'Разное',
 				link: '/gallery/other/',
+				poster: await getPoster('other'),
 			},
 			{
 				title: 'Moto life',
 				link: '/gallery/moto/',
+				poster: await getPoster('moto'),
 			},
 		],
 	},
